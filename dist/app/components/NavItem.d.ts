ac7f2936ba138221f5d544c0d898c7269c20faf2
CJ Gammon <cjgammon@missys-mbp-2.lan>
2023-01-09 02:06:01 -0600

    build

---
+++ dist/app/components/NavItem.d.ts
@@ -0,0 +1,5 @@
+import { LitElement, CSSResult } from "lit";
+export declare class ANavItem extends LitElement {
+    static get styles(): CSSResult;
+    render(): import("lit-html").TemplateResult<1>;
+}
---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+import "./styles.scss";
+import "./app/components/NavItem";
